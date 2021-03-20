5fcd922ca2b79ef0c7d3a6684dcb030e47324f8e
Sembenevi <guilherme.sembeneli@gmail.com>
2021-03-20 20:58:47 -0300

    Finalizado componente footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,24 @@
-import footerImg from '../../assets/footerlogo.png'
-import { Container } from './style'
-
+import footerImg from "../../assets/footerlogo.png";
+import { Container } from "./style";
+import { Link as LinkScroll } from "react-scroll";
 export default function Footer() {
-    return (
-        <Container>
-        <img src={footerImg} alt="Foto do servidor balirp"/>
-        <div>
-          <p>Venha criar a sua história!</p>
-          <span>Duvida? Entre no discord e abra um ticket.</span>
-        </div>
+  return (
+    <Container>
+      <img src={footerImg} alt="Foto do servidor balirp" />
+      <div>
+        <p>Venha criar a sua história!</p>
+        <span>Duvida? Entre no discord e abra um ticket.</span>
+      </div>
+      <LinkScroll
+        className="buttontop"
+        to="header"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        <i className="fas fa-arrow-up"></i>
+      </LinkScroll>
     </Container>
-    )
+  );
 }
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -4,6 +4,7 @@ export const Container = styled.footer`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 
   img {
     width: 10rem;
@@ -19,4 +20,23 @@ export const Container = styled.footer`
   span {
     color: #7c7c7c;
   }
+
+  .buttontop{
+        position: absolute;
+        right: 2rem;
+        bottom: 2rem;
+        z-index: 9999;
+        background: #fff;
+        border: none;
+        border-radius: 100%;
+        padding: 1rem;
+        width: 2rem;
+        height: 2rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        box-shadow: 0 0 1em rgba(0, 0, 0, 0.479);
+        color: #e05656;
+    }
 `;
