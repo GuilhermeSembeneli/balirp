0a54b970f6382623541ae5dc69001f49e05655db
Sembenevi <guilherme.sembeneli@gmail.com>
2021-03-24 11:08:01 -0300

    Ajustado o hook useLogin

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -2,10 +2,5 @@ import React from "react";
 import { api } from "../services/api";
 
 export default function useLogin() {
-   React.useEffect(() => {
-        api.post('createuser', {
-            email: 'teste',
-            password: 'test'
-        })
-   }, [])
+
 }
